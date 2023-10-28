603368bb20ed924953290d947f834d406e4be262
Pavel Churzin <pavel@pchurzin.ru>
2023-10-28 17:21:15 +0300

    knob outputs

--- src/Vpc40.cpp
+++ src/Vpc40.cpp
@@ -19,7 +19,22 @@ struct Vpc40Module : Module {
         NUM_INPUTS
     };
     enum OutputIds {
-        DEVICE_1_OUTPUT,
+        DEVICE_KNOB_1_OUTPUT,
+        DEVICE_KNOB_2_OUTPUT,
+        DEVICE_KNOB_3_OUTPUT,
+        DEVICE_KNOB_4_OUTPUT,
+        DEVICE_KNOB_5_OUTPUT,
+        DEVICE_KNOB_6_OUTPUT,
+        DEVICE_KNOB_7_OUTPUT,
+        DEVICE_KNOB_8_OUTPUT,
+        TRACK_KNOB_1_OUTPUT,
+        TRACK_KNOB_2_OUTPUT,
+        TRACK_KNOB_3_OUTPUT,
+        TRACK_KNOB_4_OUTPUT,
+        TRACK_KNOB_5_OUTPUT,
+        TRACK_KNOB_6_OUTPUT,
+        TRACK_KNOB_7_OUTPUT,
+        TRACK_KNOB_8_OUTPUT,
         NUM_OUTPUTS
     };
     enum LightIds {
@@ -36,18 +51,26 @@ struct Vpc40Module : Module {
     uint8_t sysExDeviceId = -1;
 
     float device1 = 0.f;
+    uint8_t bank = 0;
+
+    // track knob values 
+    float trackKnobs[PORT_MAX_CHANNELS * C_KNOB_NUM] = {0};
+    // device knob values 
+    float deviceKnobs[PORT_MAX_CHANNELS * C_KNOB_NUM] = {0};
 
     Vpc40Module() {
         config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
         configButton(RESET_PARAM, "Reset");
         configButton(TEST_PARAM, "Test");
-        configOutput(DEVICE_1_OUTPUT, "Device1");
+        for (int i = 0; i < C_KNOB_NUM; i++) {
+            configOutput(DEVICE_KNOB_1_OUTPUT + i, string::f("Device %d", i + 1));
+            configOutput(TRACK_KNOB_1_OUTPUT + i, string::f("Track %d", i + 1));
+        }
         ioPort.input = &midiInput;
         ioPort.output = &midiOutput;
     }
 
     void process(const ProcessArgs &args) override {
-        outputs[DEVICE_1_OUTPUT].setVoltage(10.f * clamp(device1, 0.f, 1.f));
         if(resetButtonTrigger.process(params[RESET_PARAM].getValue())) {
             inquireDevice();
         }
@@ -74,40 +97,57 @@ struct Vpc40Module : Module {
             }
             if (inboundMidi.getStatus() == STATUS_CC) {
                 setCc(args.frame, inboundMidi.getChannel(), inboundMidi.getNote(), inboundMidi.getValue());
-                if(inboundMidi.getNote() == C_DEVICE_KNOB_1) {
-                    device1 = inboundMidi.getValue() / 127.f;
+                uint8_t cc = inboundMidi.getNote();
+                if (cc >= C_DEVICE_KNOB_1 && cc <= C_DEVICE_KNOB_8) {
+                    deviceKnobs[bank * C_KNOB_NUM + cc - C_DEVICE_KNOB_1] = inboundMidi.getValue() / 127.f;
+                }
+                if (cc >= C_TRACK_KNOB_1 && cc <= C_TRACK_KNOB_8) {
+                    trackKnobs[bank * C_KNOB_NUM + cc - C_TRACK_KNOB_1] = inboundMidi.getValue() / 127.f;
+                }
+            }
+
+            for (uint8_t c = 0; c < PORT_MAX_CHANNELS; c++) {
+                // update track knob outputs
+                for(int k = 0; k < C_KNOB_NUM; k++) {
+                    if (!getOutput(TRACK_KNOB_1_OUTPUT + k).isConnected()) continue;
+                    getOutput(TRACK_KNOB_1_OUTPUT + k).setVoltage(10.f * clamp(trackKnobs[k], 0.f, 1.f), c);
+                }
+                // update device knob outputs
+                for(int k = 0; k < C_KNOB_NUM; k++) {
+                    if (!getOutput(DEVICE_KNOB_1_OUTPUT + k).isConnected()) continue;
+                    getOutput(DEVICE_KNOB_1_OUTPUT + k).setVoltage(10.f * clamp(deviceKnobs[k], 0.f, 1.f), c);
                 }
             }
         }
     }
 
-    void setCc(int64_t frame, uint8_t channel, uint8_t cc, uint8_t value) {
+    void setCc(int64_t frame, uint8_t midiChannel, uint8_t cc, uint8_t value) {
         rack::midi::Message msg;
         msg.setFrame(frame);
-msg.setChannel(channel);
+        msg.setChannel(midiChannel);
         msg.setNote(cc);
         msg.setStatus(STATUS_CC);
         msg.setValue(value);
         midiOutput.sendMessage(msg);
     }
 
-    void setLedOn(int64_t frame, uint8_t channel, uint8_t note) {
-        setLedOn(frame, channel, note, LED_ON);
+    void setLedOn(int64_t frame, uint8_t midiChannel, uint8_t note) {
+        setLedOn(frame, midiChannel, note, LED_ON);
     }
 
-    void setLedOff(int64_t frame, uint8_t channel, uint8_t note) {
+    void setLedOff(int64_t frame, uint8_t midiChannel, uint8_t note) {
         rack::midi::Message msg;
         msg.setFrame(frame);
-        msg.setChannel(channel);
+        msg.setChannel(midiChannel);
         msg.setNote(note);
         msg.setStatus(STATUS_NOTE_OFF);
         msg.setValue(0);
         midiOutput.sendMessage(msg);
     }
-    void setLedOn(int64_t frame, uint8_t channel, uint8_t note, uint8_t ledValue) {
+    void setLedOn(int64_t frame, uint8_t midiChannel, uint8_t note, uint8_t ledValue) {
         rack::midi::Message msg;
         msg.setFrame(frame);
-        msg.setChannel(channel);
+        msg.setChannel(midiChannel);
         msg.setNote(note);
         msg.setStatus(STATUS_NOTE_ON);
         msg.setValue(ledValue);
@@ -190,7 +230,14 @@ struct Vpc40Widget : ModuleWidget {
         addParam(createLightParamCentered<VCVLightButton<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(101.441, 33.679)), module, Vpc40Module::RESET_PARAM, Vpc40Module::RESET_LIGHT));
         addParam(createLightParamCentered<VCVLightButton<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(101.441, 63.679)), module, Vpc40Module::TEST_PARAM, Vpc40Module::TEST_LIGHT));
 
-        addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(120, 70)), module, Vpc40Module::DEVICE_1_OUTPUT));
+        for(int i = 0; i < C_KNOB_NUM / 2; i++) {
+            addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(150 + 10 * i, 10)), module, Vpc40Module::TRACK_KNOB_1_OUTPUT + i));
+            addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(150 + 10 * i, 40)), module, Vpc40Module::DEVICE_KNOB_1_OUTPUT + i));
+        }
+        for(int i = 0; i < C_KNOB_NUM / 2; i++) {
+            addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(150 + 10 * i, 20)), module, Vpc40Module::TRACK_KNOB_5_OUTPUT + i));
+            addOutput(createOutputCentered<ThemedPJ301MPort>(mm2px(Vec(150 + 10 * i, 50)), module, Vpc40Module::DEVICE_KNOB_5_OUTPUT + i));
+        }
     }
 };
 
